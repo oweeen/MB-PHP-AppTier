8c0b609aa5d12cc53c25343818954fc60a7098aa
oweeen <owenwynn@amazon.com>
2016-12-06 21:41:39 -0800

    Update db_new.php

--- source/db_new.php
+++ source/db_new.php
@@ -21,7 +21,156 @@
   $database = mysqli_select_db($connection, DB_DATABASE);
   /* Ensure that the Employees table exists. */
   VerifyEmployeesTable($connection, DB_DATABASE);
+  /* If input fields are populated<?php include "../inc/dbinfo.inc"; ?>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Octank Homepage!</title>
+  <style>
+    body {
+      color: #ffffff;
+      background-color: #0188cc;
+      font-family: Arial, sans-serif;
+      font-size: 14px;
+    }
+    
+    h1 {
+      font-size: 500%;
+      font-weight: normal;
+      margin-bottom: 0;
+    }
+    
+    h2 {
+      font-size: 200%;
+      font-weight: normal;
+      margin-bottom: 0;
+    }
+  </style>
+</head>
+<body>
+  <h4>This page is being served from region: 
+<?php
+    $curl = curl_init();
+    curl_setopt ($curl, CURLOPT_URL, "http://169.254.169.254/latest/meta-data/placement/availability-zone");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec ($curl);
+    curl_close ($curl);
+    $result = substr_replace($result, "", -1);
+    print $result;
+?>
+  </h4>
+
+  <div align="left">
+
+    <h2Using this page you can interact with the database</h2>
+    <p></p>
+  </div>
+  
+  
+  <?php
+  /* Connect to MySQL and select the database. */
+  $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+  if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  $database = mysqli_select_db($connection, DB_DATABASE);
+  /* Ensure that the Employees table exists. */
+  VerifyEmployeesTable($connection, DB_DATABASE);
   /* If input fields are populated, add a row to the Employees table. */
+  $employee_name = htmlentities($_POST['Name']);
+  $employee_address = htmlentities($_POST['Address']);
+  if (strlen($employee_name) || strlen($employee_address)) {
+    AddEmployee($connection, $employee_name, $employee_address);
+  }
+?>
+
+<!-- Input form -->
+<form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
+  <table border="0">
+    <tr>
+      <td>Product category</td>
+      <td>Product item</td>
+    </tr>
+    <tr>
+      <td>
+        <input type="text" name="Category" maxlength="45" size="30" />
+      </td>
+      <td>
+        <input type="text" name="Item" maxlength="90" size="60" />
+      </td>
+      <td>
+        <input type="submit" value="Add Data" />
+              </td>
+    </tr>
+  </table>
+</form>
+
+<!-- Display table data. -->
+<table border="1" cellpadding="2" cellspacing="2">
+  <tr>
+    <td>ID</td>
+    <td>Category</td>
+    <td>Item</td>
+  </tr>
+
+<?php
+$result = mysqli_query($connection, "SELECT * FROM Employees");
+while($query_data = mysqli_fetch_row($result)) {
+  echo "<tr>";
+  echo "<td>",$query_data[0], "</td>",
+       "<td>",$query_data[1], "</td>",
+       "<td>",$query_data[2], "</td>";
+  echo "</tr>";
+}
+?>
+
+</table>
+
+<!-- Clean up. -->
+<?php
+  mysqli_free_result($result);
+  mysqli_close($connection);
+?>
+  
+
+<div align="left">
+    Powered by - <img src="http://cdn.octank.owcloud.xyz/AWS_logo.png" width="25%" height="25%" />
+  </div>
+</body>
+</html>
+
+<?php
+/* Add an employee to the table. */
+function AddEmployee($connection, $name, $address) {
+   $n = mysqli_real_escape_string($connection, $name);
+   $a = mysqli_real_escape_string($connection, $address);
+   $query = "INSERT INTO `Employees` (`Name`, `Address`) VALUES ('$n', '$a');";
+   if(!mysqli_query($connection, $query)) echo("<p>Error adding employee data.</p>");
+}
+/* Check whether the table exists and, if not, create it. */
+function VerifyEmployeesTable($connection, $dbName) {
+  if(!TableExists("Employees", $connection, $dbName))
+  {
+     $query = "CREATE TABLE `Employees` (
+         `ID` int(11) NOT NULL AUTO_INCREMENT,
+         `Name` varchar(45) DEFAULT NULL,
+         `Address` varchar(90) DEFAULT NULL,
+         PRIMARY KEY (`ID`),
+         UNIQUE KEY `ID_UNIQUE` (`ID`)
+       ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1";
+     if(!mysqli_query($connection, $query)) echo("<p>Error creating table.</p>");
+  }
+}
+/* Check for the existence of a table. */
+function TableExists($tableName, $connection, $dbName) {
+  $t = mysqli_real_escape_string($connection, $tableName);
+  $d = mysqli_real_escape_string($connection, $dbName);
+  $checktable = mysqli_query($connection,
+      "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$
+d'");
+  if(mysqli_num_rows($checktable) > 0) return true;
+  return false;
+}
+?>
+, add a row to the Employees table. */
   $employee_name = htmlentities($_POST['Name']);
   $employee_address = htmlentities($_POST['Address']);
   if (strlen($employee_name) || strlen($employee_address)) {
